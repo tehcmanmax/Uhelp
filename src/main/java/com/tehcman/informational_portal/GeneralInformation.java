a4a1115306b84e3ac5292f74d81d98e19c06aba6
Danila <danpry@st.amu.edu.pl>
2022-06-02 14:20:28 +0200

    Refactored GeneralInformation class IGeneralInformation interface was returned

--- src/main/java/com/tehcman/informational_portal/GeneralInformation.java
+++ src/main/java/com/tehcman/informational_portal/GeneralInformation.java
@@ -1,39 +1,13 @@
 package com.tehcman.informational_portal;
 
-public class GeneralInformation {
+public class GeneralInformation implements IGeneralInformation{
     private final String generalInformation;
 
     public GeneralInformation() {
         generalInformation = "Russian invasion of Ukraine 2022 General information: \n\n" +
                 "On 24 February 2022, Russia invaded Ukraine, marking a steep escalation of the Russo-Ukrainian War, " +
                 "which had begun in 2014. The invasion has caused Europe's fastest-growing refugee crisis since World War II," +
-                " with more than 6.7 million Ukrainians fleeing the country[19] and a third of the population displaced.\n\n" +
-                "At the start of the war in 2014, Russia invaded and annexed Crimea, and Russian-backed separatists seized part of the Donbas," +
-                " the south-eastern region of Ukraine (consisting of Luhansk and Donetsk oblasts), sparking a regional war. " +
-                "In 2021, Russia began a large military build-up along its border with Ukraine, amassing up to 190,000 troops and their equipment." +
-                " In a televised address shortly before the invasion, Russian president Vladimir Putin espoused irredentist views," +
-                " challenged Ukraine's right to statehood, and falsely accused Ukraine of being governed by neo-Nazis who persecute the ethnic Russian minority." +
-                " Putin also alleged that eastward expansion by the North Atlantic Treaty Organization (NATO) threatens Russia's national security, something NATO has disputed." +
-                " Russia demanded NATO stop expanding and permanently bar Ukraine from ever joining the alliance." +
-                " Multiple nations accused Russia of planning to attack or invade Ukraine, which Russian officials repeatedly denied as late as 23 February 2022.\n\n" +
-                "On 21 February 2022, Russia recognised the Donetsk People's Republic and the Luhansk People's Republic," +
-                " two self-proclaimed breakaway statelets in Donbas controlled by pro-Russian separatists.[35] The following day," +
-                " the Federation Council of Russia authorised the use of military force abroad, and Russian troops overtly entered" +
-                " both territories.[36] The invasion began on the morning of 24 February,[37] when Putin announced a" +
-                " \"special military operation\" to \"demilitarise and denazify\" Ukraine. Minutes later, missiles and airstrikes hit across" +
-                " Ukraine, including the capital Kyiv, shortly followed by a large ground invasion from multiple directions." +
-                " Ukrainian president Volodymyr Zelenskyy enacted martial law and a general mobilisation of all male " +
-                "Ukrainian citizens between 18 and 60, who were banned from leaving the country.\n\n" +
-                "The invasion has received widespread international condemnation. The United Nations General Assembly" +
-                " passed a resolution condemning the invasion and demanding a full withdrawal of Russian forces." +
-                " The International Court of Justice ordered Russia to suspend military operations and the Council of" +
-                " Europe expelled Russia. Many countries imposed sanctions on Russia, which have affected the economies" +
-                " of Russia and the world, and provided humanitarian and military aid to Ukraine. Protests occurred around " +
-                "the world; those in Russia were met with mass arrests and increased media censorship, including" +
-                " banning the use of the words \"war\" and \"invasion\". Numerous companies withdrew their products " +
-                "and services from Russia and Belarus, and Russian state-funded media were banned from broadcasting" +
-                " and removed from online platforms. The International Criminal Court has opened an investigation into" +
-                " crimes against humanity in Ukraine since 2013, as well as war crimes in the 2022 invasion.";
+                " with more than 6.7 million Ukrainians fleeing the country[19] and a third of the population displaced.\n\n";
     }
 
     public String getGeneralInformation() {
