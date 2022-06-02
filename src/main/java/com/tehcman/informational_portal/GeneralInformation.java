fae91abb527db8438c02adfd813cb2e71e59b8f5
Danila <danpry@st.amu.edu.pl>
2022-06-02 14:36:15 +0200

    GeneralInformation field text was changed

--- src/main/java/com/tehcman/informational_portal/GeneralInformation.java
+++ src/main/java/com/tehcman/informational_portal/GeneralInformation.java
@@ -7,7 +7,7 @@ public class GeneralInformation implements IGeneralInformation{
         generalInformation = "Russian invasion of Ukraine 2022 General information: \n\n" +
                 "On 24 February 2022, Russia invaded Ukraine, marking a steep escalation of the Russo-Ukrainian War, " +
                 "which had begun in 2014. The invasion has caused Europe's fastest-growing refugee crisis since World War II," +
-                " with more than 6.7 million Ukrainians fleeing the country[19] and a third of the population displaced.\n\n";
+                " with more than 6.7 million Ukrainians fleeing the country and a third of the population displaced.\n\n";
     }
 
     public String getGeneralInformation() {
