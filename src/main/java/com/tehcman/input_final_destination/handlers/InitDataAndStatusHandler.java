fa2a9094dcf4f2050dc42c5e73583a768315f6c6
tehcman <makdmy@st.amu.edu.pl>
2022-07-31 18:14:27 +0200

    implementation fix of the InitDataAndStatusHandler class

--- src/main/java/com/tehcman/input_final_destination/handlers/InitDataAndStatusHandler.java
+++ src/main/java/com/tehcman/input_final_destination/handlers/InitDataAndStatusHandler.java
@@ -1,13 +1,15 @@
 package com.tehcman.input_final_destination.handlers;
 
+import com.tehcman.input_final_destination.SendMessage_factories.ISendMessageFactory;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
 @Component
-public class InitDataAndStatusHandler implements IHandler<Message> {
+public class InitDataAndStatusHandler implements ISendMessageFactory {
 
     @Override
-    public void handle(Message message) {
-
+    public SendMessage createSendMessage(Message message) {
+        return null;
     }
 }
